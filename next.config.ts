ea257c51904c4091ecb82453efe19a5cbcf3574d
haile121 <haileag8@gmail.com>
2026-06-01 20:13:06 +0300

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+// next.config.js
+module.exports = {
+  allowedDevOrigins: ['delicacy-affront-crayfish.ngrok-free.dev'],
+}
+
+export default nextConfig;
