238b2c7fe59f503d45e8bc60bbed9a37163cecc6
haile121 <haileag8@gmail.com>
2026-07-13 22:49:00 +0300

    chore: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   /* config options here */
 };
 
 // next.config.js
 module.exports = {
-  allowedDevOrigins: ['delicacy-affront-crayfish.ngrok-free.dev'],
-}
+  allowedDevOrigins: ["delicacy-affront-crayfish.ngrok-free.dev"],
+};
 
 export default nextConfig;
