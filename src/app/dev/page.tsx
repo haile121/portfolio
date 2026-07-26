f8580c12aeeb3980973624145ff021d754799427
haile121 <haileag8@gmail.com>
2026-07-26 13:36:35 +0300

    Add dev portfolio route page

---
+++ src/app/dev/page.tsx
@@ -0,0 +1,9 @@
+import { DevPortfolio } from "../../components/dev/DevPortfolio";
+
+export default function DevPage() {
+  return (
+    <main className="w-full min-h-screen relative overflow-hidden bg-bg">
+      <DevPortfolio />
+    </main>
+  );
+}
