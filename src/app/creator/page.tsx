773befd7537702bb4f7b2f9d8d4105927ed2fa4f
haile121 <haileag8@gmail.com>
2026-07-26 13:38:34 +0300

    Add creator portfolio route page

---
+++ src/app/creator/page.tsx
@@ -0,0 +1,9 @@
+import { CreatorPortfolio } from "../../components/creator/CreatorPortfolio";
+
+export default function CreatorPage() {
+  return (
+    <main className="w-full min-h-screen relative overflow-hidden bg-bg">
+      <CreatorPortfolio />
+    </main>
+  );
+}
